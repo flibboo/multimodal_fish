cf7d99b3165c8f63f42c9af2d26dc05f2d32c02b
flibboo <95918513+flibboo@users.noreply.github.com>
2022-01-27 11:27:00 +0100

    Add files via upload

---
+++ logistic_regression.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from IPython import embed
+import numpy as np
+from functions import *
+# Open file
+training_data = pd.read_hdf("training_dataframe.hf")
+all_fish = np.array(training_data.columns)
+
+# Parse through fish
+for fish in all_fish:
+    flattened_fish = flatten_fish(fish, training_data)
+
+    embed()
+    quit()
+    # At this point you can do the Logistic Regression
+
+    fish_regression(fish, curr_fish_flattened_array)
